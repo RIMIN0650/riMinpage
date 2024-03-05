2cef5d010b9961cf22a49cbd135ee3f4751518f7
RIMIN <chris0540@naver.com>
2024-03-05 20:12:17 +0900

    [권민석]  - riMinpage 프로젝트 생성  - sts 초기 설정 확인

---
+++ src/test/java/com/rimin/riMinpage/RiMinpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimin.riMinpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiMinpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
